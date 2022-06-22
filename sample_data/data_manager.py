7441231d0c6e9b91d76390fa6f0e1093acf62595
MonikaMarkulis <markulismonika@gmail.com>
2022-06-22 09:12:46 +0200

    Add route question<gestion_id>

--- sample_data/data_manager.py
+++ sample_data/data_manager.py
@@ -19,7 +19,6 @@ def get_question_by_id(id):
         if item['id'] == id:
             return item
 
-
 def get_quetion_and_answers(id):
 
     question = get_question_by_id(id)
@@ -30,3 +29,14 @@ def get_quetion_and_answers(id):
             list_of_answers.append(answer)
     question.setdefault("answers", list_of_answers)
     return question
+
+def generate_id():
+
+    _list = connection.read_file("question.csv")
+    generated_id = len(_list)+1
+    return generated_id
+
+def write_message(row):
+    connection.write_file(row, "question.csv")
+    pass
+
--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template, jsonify
-from sample_data import data_manager
+from sample_data import data_manager, connection
+import util
 
 app = Flask(__name__)
 
@@ -26,21 +27,20 @@ def display_question():
 @app.route("/add_question", methods=['POST', 'GET'])
 def add_question():
     if request.method == 'POST':
-        id = '3' #len listy +1?
+        id = data_manager.generate_id()
         submission_time = util.get_time()
         # view_number = request.form[]
         # vote_number = request.form[]
-        title = request.form['question_title']
-        message = request.form['question']
+        title = request.form['title']
+        message = request.form['message']
         # image = request.form[]
-        questions = connection.read_file('question') 
+        questions = connection.read_file('question.csv')
         data_to_save = [id,submission_time,"view_number","vote_number",title,message,"image"]
-
-        connection.write_file(data_to_save, 'question.csv')
-        data = connection.read_file('question')
+        data_manager.write_message(data_to_save)
+        data = connection.read_file('question.csv')
 
         return render_template('list.html', data=data, title=title, id=id)
-    return render_template('add_question.html')
+    return render_template('ask_question.html')
 
 @app.route('/question/<string:id>')
 def get_guestion_by_id(id):
