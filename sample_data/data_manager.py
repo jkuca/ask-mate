eb2e8536db9680286a98241dfe7085f6ea925661
Lukasz Wilanski <lukasz.wilanski@gmail.com>
2022-07-07 03:30:13 +0200

    added html template to display 5 questions on landing website

--- sample_data/data_manager.py
+++ sample_data/data_manager.py
@@ -12,13 +12,10 @@ def get_sorted_questions(cursor, parameter, value_parameter):
         ORDER BY {parameter} {value_parameter}"""
     cursor.execute(query)
     return cursor.fetchall()
-    # questions = connection.read_file("question.csv")
-    # questions.sort(key=sorting)
-    # return questions
 
 
 @database_common.connection_handler
-def get_sorted_comments(cursor, parameter, id:str):
+def get_sorted_comments(cursor, parameter, id: str):
     query = f"""
         SELECT *
         FROM comment
@@ -29,14 +26,6 @@ def get_sorted_comments(cursor, parameter, id:str):
 
 @database_common.connection_handler
 def get_question_by_id(cursor, id):
-<<<<<<< HEAD
-    query = f"""
-            SELECT *
-            FROM question
-            WHERE id = {id}"""
-    cursor.execute(query)
-    return cursor.fetchall()
-=======
     print(id)
     query = """
             SELECT *
@@ -44,7 +33,6 @@ def get_question_by_id(cursor, id):
             WHERE id = %(id)s"""
     cursor.execute(query, {"id": id})
     return cursor.fetchone()
->>>>>>> adam
 
 
 @database_common.connection_handler
@@ -58,17 +46,6 @@ def get_answer_by_id(cursor, id):
     return cursor.fetchall()
 
 
-<<<<<<< HEAD
-def write_message_update(row):
-    connection.update_file(row, "question.csv")
-
-
-def get_edit_question(id_post, title, message):
-    data_of_question = get_question_by_id(id_post)
-    data_to_save = {'id': data_of_question['id'], 'submission_time': data_of_question['submission_time'], 'view_number': data_of_question['view_number'],
-                    'vote_number': data_of_question['vote_number'], 'title': title, 'message': message, "image": ""}
-    write_message_update(data_to_save)
-=======
 @database_common.connection_handler
 def get_edit_question_message(cursor, id_post, message):
     query = f"""
@@ -77,13 +54,10 @@ def get_edit_question_message(cursor, id_post, message):
                     WHERE id = %(id)s
                     """
     cursor.execute(query, {"id": id_post, "message": message})
->>>>>>> adam
 
 
 def generate_id(file):
-    data = connection.read_file(file)
-    generated_id = int(data[-1]['id'])+1
-    return generated_id
+    pass
 
 
 @database_common.connection_handler
@@ -102,52 +76,33 @@ def add_new_question(cursor, title, message):
 
 
 @database_common.connection_handler
-def add_new_comment(cursor, message, id_question = 'NULL', id_answer = 'NULL'):
+def add_new_comment(cursor, message, id_question='NULL', id_answer='NULL'):
     submission_time = util.get_time()
-<<<<<<< HEAD
-
-
-def add_new_answer(question_id, message):
-    data_to_save = connection.get_row("answer.csv")
-    data_to_save['id'] = generate_id("answer.csv")
-    data_to_save['submission_time'] = util.get_time()
-    data_to_save['message'] = message
-    data_to_save['question_id'] = question_id
-
-    write_new_row(data_to_save, "answer.csv")
-=======
     query = f"""
                     INSERT INTO comment
                     VALUES (DEFAULT, {id_question}, {id_answer}, %(message)s, '{submission_time}', 0)
                     """
     cursor.execute(query, {'message': message})
->>>>>>> adam
 
 
 def count_visits(id):
-    data_of_question = get_question_by_id(id)
-    data_of_question['view_number'] = int(data_of_question['view_number']) + 1
-    write_message_update(data_of_question)
+    pass
 
 
 def delete_row(id, directory):
-    connection.delete_row({'id': id}, directory)
+    pass
 
 
 def get_votes(data_of_question):
-    return int(data_of_question['vote_number'])
+    pass
 
 
 def count_votes_up(id):
-    data_of_question = get_question_by_id(id)
-    data_of_question['vote_number'] = get_votes(data_of_question) + 1
-    write_message_update(data_of_question)
+    pass
 
 
 def count_votes_down(id):
-    data_of_question = get_question_by_id(id)
-    data_of_question['vote_number'] = get_votes(data_of_question) - 1
-    write_message_update(data_of_question)
+    pass
 
 
 @database_common.connection_handler
