061b2c51918ea40af7364366f073708fb4642fe5
Lukasz Wilanski <lukasz.wilanski@gmail.com>
2022-08-01 14:44:48 +0200

    fix serching and add simple login

--- server.py
+++ server.py
@@ -1,17 +1,4 @@
-from flask import Flask, render_template
-from bonus_questions import SAMPLE_QUESTIONS
-
-app = Flask(__name__)
-
-
-@app.route("/bonus-questions")
-def main():
-    return render_template('bonus_questions.html', questions=SAMPLE_QUESTIONS)
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
-from flask import Flask, request, render_template, jsonify, redirect, url_for
+from flask import Flask, request, render_template, jsonify, redirect, url_for, session
 from sample_data import data_manager
 
 app = Flask(__name__)
@@ -23,7 +10,7 @@ app = Flask(__name__)
 ###################################################
 
 
-@app.route("/")
+@app.route("/", methods=['POST', 'GET'])
 def home():
     latest_questions = data_manager.get_latest_questions(5)
     return render_template('home.html', questions=latest_questions)
@@ -35,12 +22,11 @@ def home():
 
 @app.route("/list", methods=['POST', 'GET'])
 def get_all_questions_sorted_by_submission_time():
-    filter_question = request.args.get('filter_question')
+    order = request.args.get('filter_question')
     direction = request.args.get('direction')
-    print(filter_question, "   -   ", direction)
-    if filter_question and direction:
+    if order and direction:
         questions = data_manager.get_sorted_questions(
-            filter_question, direction)
+            order, direction)
     else:
         questions = data_manager.get_sorted_questions('id', 'DESC')
     return render_template('list.html', questions=questions)
@@ -57,14 +43,18 @@ def get_question(question_id):
     data_manager.count_visits(question_id, question['view_number'])
     comments = data_manager.get_sorted_comments(
         parameter='question_id', id=question_id)
-    # , comments=comments)
-    return render_template('display_question.html', data=question, answers=answers)
+    comments_answer = []
+    for i in answers:
+        asa = data_manager.get_sorted_comments(
+            parameter='answer_id', id=str(i['id']))
+        comments_answer.append(asa)
+    return render_template('display_question.html', data=question, answers=answers, comments=comments, comments_answer=comments_answer)
 
 ########### Question Options #######################
 
 
 ###### Add Question #############
-@app.route("/add_question", methods=['POST', 'GET'])
+@ app.route("/add_question", methods=['POST', 'GET'])
 def add_question():
     if request.method == 'POST':
         title = request.form.get('title')
@@ -76,7 +66,7 @@ def add_question():
 
 
 ######## Edit Question #############
-@app.route("/question/<string:id_post>/edit", methods=["POST", "GET"])
+@ app.route("/question/<string:id_post>/edit", methods=["POST", "GET"])
 def edit_question(id_post):
     if request.method == "POST":
         message = request.form.get("message")
@@ -89,26 +79,14 @@ def edit_question(id_post):
     else:
         data_of_question = data_manager.get_question_by_id(id_post)
 
-    return render_template("edit.html", data=data_of_question)
+    return render_template("edit.html", data=data_of_question, direction='question')
 
 
 ########### Delete Question ############
-@app.route("/question/<string:id_post>/delete")
+@ app.route("/question/<string:question_id>/delete")
 def delete_question(question_id):
     data_manager.delete_question(question_id)
     return redirect('/list')
-
-
-########## Add Comment to Question ###############
-@app.route("/question/<string:id_question>/new-comment", methods=['POST', 'GET'])
-def add_new_comment_question(id_question):
-    if request.method == 'POST':
-        message = request.form.get('comment')
-        data_manager.add_new_comment(message, id_question=id_question)
-        return redirect(url_for('get_question', question_id=id_question))
-    else:
-        question = data_manager.get_question_by_id("2")
-        return render_template('comment.html', data=question)
 
 
 #############################
@@ -117,7 +95,7 @@ def add_new_comment_question(id_question):
 
 
 ######### Vote Up ########
-@app.route("/question/<string:question_id>/vote-up")
+@ app.route("/question/<string:question_id>/vote-up")
 def vote_question_up(question_id):
     question = data_manager.get_question_by_id(question_id)
     data_manager.count_votes_up(question_id, question['vote_number'])
@@ -127,7 +105,7 @@ def vote_question_up(question_id):
 ###### Vote Down ########
 
 
-@app.route("/question/<string:question_id>/vote-down")
+@ app.route("/question/<string:question_id>/vote-down")
 def vote_question_down(question_id):
     question = data_manager.get_question_by_id(question_id)
     data_manager.count_votes_down(question_id, question['vote_number'])
@@ -141,11 +119,11 @@ def vote_question_down(question_id):
 ######## Add answer to Question ################
 
 
-@app.route("/question/<string:id_post>/new-answer", methods=['POST', 'GET'])
+@ app.route("/question/<string:id_post>/new-answer", methods=['POST', 'GET'])
 def add_answer(id_post):
     if request.method == 'POST':
         message = request.form.get('message')
-        data_manager.add_new_answer(id_post, message)
+        data_manager.add_new_answer(message, id_post)
         return redirect(url_for('get_question', question_id=id_post))
     return render_template('add_answer.html', data=id_post)
 
@@ -153,7 +131,7 @@ def add_answer(id_post):
 # template edit_answer - redirect from the answer page
 
 ############# Edit Answer ####################
-@app.route('/answer/<string:answer_id>/edit', methods=['GET', 'POST'])
+@ app.route('/answer/<string:answer_id>/edit', methods=['GET', 'POST'])
 def edit_answer(answer_id):
     answer = data_manager.get_one_answer_by_id(answer_id)
     if request.method == "POST":
@@ -163,13 +141,11 @@ def edit_answer(answer_id):
     return render_template('edit_answer.html', answer=answer)
 
 
-################ Delete Answer #################
-
-
-@app.route("/answer/<string:id_answer>/delete")
-def delete_answer(answer_id):
-    data_manager.delete_answer(answer_id)
-    answer = data_manager.get_one_answer_by_id(answer_id)
+################ Delete Answer ################
+@ app.route("/answer/<string:id_answer>/delete")
+def delete_answer(id_answer):
+    answer = data_manager.get_one_answer_by_id(id_answer)
+    data_manager.delete_answer(id_answer)
     blink_url = "/question/" + str(answer['question_id'])
     return redirect(blink_url, 302)
 
@@ -178,7 +154,9 @@ def delete_answer(answer_id):
 ###############################################
 
 ############## Answer Vote Up ################
-@app.route("/answer/<string:answer_id>/vote-up")
+
+
+@ app.route("/answer/<string:answer_id>/vote-up")
 def vote_answer_up(answer_id):
     answer = data_manager.get_one_answer_by_id(answer_id)
     data_manager.count_votes_answer_up(answer_id, answer['vote_number'])
@@ -188,7 +166,7 @@ def vote_answer_up(answer_id):
 ############## Answer Vote Down ################
 
 
-@app.route("/answer/<string:answer_id>/vote-down")
+@ app.route("/answer/<string:answer_id>/vote-down")
 def vote_answer_down(answer_id):
     answer = data_manager.get_one_answer_by_id(answer_id)
     data_manager.count_votes_answer_down(answer_id, answer['vote_number'])
@@ -196,33 +174,90 @@ def vote_answer_down(answer_id):
     return redirect(blink_url, 302)
 
 
-############## Answer Add Comment ################
-@app.route("/answer/<string:id_answer>/new-comment", methods=['POST', 'GET'])
+#############################
+########## COMMENTS #########
+#############################
+
+########## Add Comment to Question ###############
+@ app.route("/question/<string:id_question>/new-comment", methods=['POST', 'GET'])
+def add_new_comment_question(id_question):
+    question = data_manager.get_question_by_id(id_question)
+    if request.method == 'POST':
+        message = request.form.get('comment')
+        data_manager.add_new_comment(message, id_question=id_question)
+        return redirect(url_for('get_question', question_id=question['id']))
+    return render_template('comment.html', data=question['id'])
+
+
+########### Add Comment to Answer ################
+@ app.route("/answer/<string:id_answer>/new-comment", methods=['POST', 'GET'])
 def add_new_comment_answer(id_answer):
+    answer = data_manager.get_one_answer_by_id(id_answer)
     if request.method == 'POST':
         message = request.form.get('comment')
         data_manager.add_new_comment(message, id_answer=id_answer)
-        return redirect(url_for('get_question', question_id=id_answer))
-    else:
-        question = data_manager.get_question_by_id("2")
-        return render_template('comment.html', data=question)
+        return redirect(url_for('get_question', question_id=answer['question_id']))
+    return render_template('comment-answer.html', data=answer)
 
-@app.route('/comment/<comment_id>/delete')
+
+########## DELETE COMMENT ################
+@ app.route('/comment/<string:comment_id>/delete')
 def delete_comment(comment_id):
+    comment = data_manager.get_comment_by_id(comment_id)
+    if comment['question_id']:
+        blink_url = "/question/" + str(comment['question_id'])
+    else:
+        answer = data_manager.get_one_answer_by_id(comment['answer_id'])
+        blink_url = "/question/" + str(answer['question_id'])
     data_manager.delete_comment(comment_id)
-    comment = get_comment_by_id(comment_id)
-    blink_url = "/question/" + str(comment['question_id'])
     return redirect(blink_url, 302)
+
+########## EDIT COMMENT ################
+
+
+@ app.route("/comment/<string:id_comment>/edit", methods=['POST', 'GET'])
+def edit_comment(id_comment):
+    if request.method == 'POST':
+        message = request.form.get('message')
+        data_manager.edit_comment(message, id_comment)
+        comment = data_manager.get_comment_by_id(id_comment)
+        if comment['question_id']:
+            return redirect(url_for('get_question', question_id=comment['question_id']))
+        else:
+            answer = data_manager.get_one_answer_by_id(
+                comment['answer_id'])
+            return redirect(url_for('get_question', question_id=answer['question_id']))
+    comment = data_manager.get_comment_by_id(id_comment)
+    return render_template('edit.html', data=comment, direction='comment')
 
 ###########################################
 ########### Serch Bar #####################
 ###########################################
 
-@app.route('/search')
+
+@ app.route('/search', methods=['POST', 'GET'])
 def search_result():
-    search_phrase = request.args.get('search_phrase')
+    search_phrase = request.form['title']
     questions = data_manager.search_questions(search_phrase)
     return render_template('search.html', search_phrase=search_phrase, questions=questions)
+
+
+# Login base logic- to change
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            session['logged_in'] = True
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+def logout():
+    session
 
 
 if __name__ == "__main__":
