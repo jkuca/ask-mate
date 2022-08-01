6fab9f950829ce90018685bedd10df4c0befccd3
Lukasz Wilanski <lukasz.wilanski@gmail.com>
2022-08-01 23:53:45 +0200

    add logic to logout based on session

--- server.py
+++ server.py
@@ -262,6 +262,14 @@ def login():
     return render_template('login.html', msg=msg)
 
 
+@app.route('/logout')
+def logout():
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('username', None)
+    return redirect(url_for('login'))
+
+
 if __name__ == "__main__":
     app.run(
         debug=True,  # Allow verbose error reports
